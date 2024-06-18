f2704314d10bb3af321499a3aed610cd09d848d1
techlism <2021itb049.kundan@students.iiests.ac.in>
2024-06-18 10:30:11 +0530

    removing splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,6 @@
 import "../global.css";
 import { Stack }from "expo-router";
-import * as SplashScreen from 'expo-splash-screen';
-SplashScreen.preventAutoHideAsync();
+
 export default function Layout() {
   return (
   <Stack initialRouteName="index">
